1ec0b22f352d06cd0a71e79e3db677d0fa6176ec
Anish Chadalavada <anish.chada@gmail.com>
2017-04-20 15:07:32 -0400

    finished workshop

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(640, 480);
+
+}
+
+function draw() {
+  rect(50, 50, 80, 80);
+  rect(mouseX,mouseY,50,50);
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill((mouseX / width) * 255, (mouseY / height) * 255, 0, 100);
+    stroke(255, 255, 255, 25);
+  }
+}
